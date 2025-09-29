1e49a6961a687749a071e7dc0f891f87bf4a87ef
Kevivin Rojas <Kevinrojaszhuma@icloud.com>
2025-09-29 17:20:09 +0200

    Adding: Static Files

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ let contacts = [
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 morgan.token('body', (req,res)=> JSON.stringify(req.body))
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
