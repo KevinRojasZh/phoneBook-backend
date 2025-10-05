d5300787f559984eeef7570b05ba731382f6cc27
Kevivin Rojas <Kevinrojaszhuma@icloud.com>
2025-10-05 16:43:31 +0200

    Fix: Port to deploy

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ const errorHendler = (error, req, res, next)=> {
 
 app.use(errorHendler)
 
-const PORT = process.env.PORT 
+const PORT = process.env.PORT || 3001
 
 
 app.listen(PORT,'0.0.0.0',()=> {
