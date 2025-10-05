6d975563f97859f36aa7df5e7a80892101a704c5
Kevivin Rojas <Kevinrojaszhuma@icloud.com>
2025-10-05 15:51:52 +0200

    Adding: line to us dist

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 
 app.use(cors())
 
-// app.use(express.static('dist'))
+app.use(express.static('dist'))
 morgan.token('body', (req,res)=> JSON.stringify(req.body))
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
